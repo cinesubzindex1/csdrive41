20517226de60dc21e9f248b78c39a402d0b86e0a
cinesubzindex1 <cinesubzindex1@gmail.com>
2024-06-11 00:47:59 +0530

    Update Layout.tsx

--- src/components/Preview/Layout.tsx
+++ src/components/Preview/Layout.tsx
@@ -36,40 +36,7 @@ export default function PreviewLayout({ data, fileType }: Props) {
       slot='preview-container'
       className='flex flex-col gap-3'
     >
-      <Card>
-        <CardHeader className='pb-0'>
-          <div
-            className={cn(
-              "flex flex-col gap-3 overflow-hidden",
-              "mobile:flex-row mobile:items-center mobile:justify-between",
-            )}
-          >
-            <h3 className='line-clamp-1 flex-grow whitespace-pre-wrap break-all'>{data.name}</h3>
-            {["text", "markdown", "code"].includes(fileType) && (
-              <div className='flex w-full items-center mobile:w-fit'>
-                <Button
-                  size={"sm"}
-                  variant={view === "markdown" ? "default" : "outline"}
-                  onClick={() => setView("markdown")}
-                  className='w-full rounded-r-none mobile:w-fit'
-                >
-                  Markdown
-                </Button>
-                <Button
-                  size={"sm"}
-                  variant={view === "raw" ? "default" : "outline"}
-                  onClick={() => setView("raw")}
-                  className='w-full rounded-l-none mobile:w-fit'
-                >
-                  Raw
-                </Button>
-              </div>
-            )}
-          </div>
-          <Separator />
-        </CardHeader>
 
-      
 
       <PreviewInformation file={data} />
     </div>
