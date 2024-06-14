e096cc496e2de0fb3a33399ec46ef595546cb947
✬Ravindu Manoj✬ ✓ <88134633+ravindu01manoj@users.noreply.github.com>
2024-06-14 11:15:40 +0530

    fix  gdrive redirect  encryption key error

--- src/app/api/internal/encrypt/route.ts
+++ src/app/api/internal/encrypt/route.ts
@@ -10,11 +10,14 @@ export async function GET(request: NextRequest) {
     const query = sp.get("q");
     if (!query) return new NextResponse("Add query parameter 'q' with the value to encrypt", { status: 400 });
 
-    const encrypted = await encryptData(query);
+    var key = sp.get("ENCRYPTION_KEY")
+    key = !key ? process.env.ENCRYPTION_KEY : key.length < 16 ? key.padEnd(16, "0") : key.length > 16 ? key.slice(0, 16) : key;
+    const encrypted = await encryptData(query,key);
 
     return NextResponse.json(
       {
         value: encrypted,
+        key
       },
       { status: 200 },
     );
